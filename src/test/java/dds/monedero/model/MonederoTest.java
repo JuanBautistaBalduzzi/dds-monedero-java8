0b6aa5cfee2715bb709c49a43b7ba552f0c8f449
JuanBautistaBalduzzi <jbalduzzi@frba.utn.edu.ar>
2022-05-09 15:37:50 -0300

    fix test

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -4,9 +4,13 @@ import dds.monedero.exceptions.MaximaCantidadDepositosException;
 import dds.monedero.exceptions.MaximoExtraccionDiarioException;
 import dds.monedero.exceptions.MontoNegativoException;
 import dds.monedero.exceptions.SaldoMenorException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MonederoTest {
@@ -20,6 +24,7 @@ public class MonederoTest {
   @Test
   void Poner() {
     cuenta.poner(1500);
+    assertEquals(1500, cuenta.getSaldo());
   }
 
   @Test
@@ -32,6 +37,8 @@ public class MonederoTest {
     cuenta.poner(1500);
     cuenta.poner(456);
     cuenta.poner(1900);
+
+    Assertions.assertEquals(3856,cuenta.getSaldo());
   }
 
   @Test
@@ -45,17 +52,39 @@ public class MonederoTest {
   }
 
   @Test
+  void sacar() {
+    cuenta.poner(10000);
+    cuenta.sacar(500);
+    Assertions.assertEquals(9500,cuenta.getSaldo());
+  }
+
+  @Test
+  void extraidoEnUnDia() {
+    cuenta.poner(10000);
+    cuenta.sacar(500);
+    cuenta.sacar(500);
+    Movimiento extraccion= new Movimiento(LocalDate.of(2022,5,7),
+        500,
+        TipoMovimiento.Extraccion);
+    cuenta.getMovimientos().add(extraccion);
+
+    assertEquals(8500,cuenta.getSaldo());
+    assertEquals(1000,cuenta.getMontoExtraidoA(LocalDate.now()));
+    assertEquals(500,cuenta.getMontoExtraidoA(LocalDate.of(2022,5,7)));
+  }
+
+  @Test
   void ExtraerMasQueElSaldo() {
     assertThrows(SaldoMenorException.class, () -> {
-          cuenta.setSaldo(90);
-          cuenta.sacar(1001);
+          cuenta.poner(90);
+          cuenta.sacar(91);
     });
   }
 
   @Test
   public void ExtraerMasDe1000() {
     assertThrows(MaximoExtraccionDiarioException.class, () -> {
-      cuenta.setSaldo(5000);
+      cuenta.poner(5000);
       cuenta.sacar(1001);
     });
   }
